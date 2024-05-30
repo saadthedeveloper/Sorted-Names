10dc091c4eddd0a0c0fa782f72e5fa14b23614af
saadthedeveloper <m.saad23145@gmail.com>
2024-05-30 00:16:48 -0700

    added decision structure

--- src/main/java/com/project/sortednames/SortedNames.java
+++ src/main/java/com/project/sortednames/SortedNames.java
@@ -19,6 +19,10 @@ public class SortedNames {
         boolean validationValue1;
         boolean validationValue2;
         boolean validationValue3;
+        String first;
+        String second;
+        String third;
+        
                 
         System.out.println("Enter a name: ");
         name1 = keyboard.nextLine();
@@ -48,8 +52,35 @@ public class SortedNames {
 
         System.out.println("Enter another name: ");
         name3 = keyboard.nextLine();
+        
+            validationValue1 = name1.equalsIgnoreCase(name2);
+            validationValue2 = name2.equalsIgnoreCase(name3);
+            validationValue3 = name3.equalsIgnoreCase(name1);
         }
-     
+        
+       if((name1).compareToIgnoreCase(name2)>0 && (name1).compareToIgnoreCase(name3) > 0)
+       {
+           if(name2.compareToIgnoreCase(name3) > 0)
+           {
+               first = name1;
+               second = name2;
+               third = name3;
+           }
+           else
+           {
+                first = name1;
+               second = name3;
+               third = name2;
+           }
+       }
+
+      
+        System.out.println("\nThe names in ascending order are:");
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        
+        keyboard.close();
     }
     
 }
